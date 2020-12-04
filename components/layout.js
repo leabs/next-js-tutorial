9e8448855132ffbfc45a69204f046fe8f5de3539
leabs <stevenleabo@gmail.com>
2020-12-04 16:04:55 -0500

    installing linting, prettier

--- components/layout.js
+++ components/layout.js
@@ -1,72 +1,105 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Steven Leabo - Web Developer'
-export const siteTitle = 'Steven Leabo - Web Developer'
+const name = "Steven Leabo - Web Developer";
+export const siteTitle = "Steven Leabo - Web Developer";
 
 export default function Layout({ children, home }) {
-  return (
-  <div>
-    <div className={styles.container} style={{ minHeight: '80vh'}}>
-      <Head>
-      < link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com/" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content=" Landing Page for Steven Leabo - Web Developer"
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Link href="/">
-              <h1 className={utilStyles.heading2Xl}>
-              Steven Leabo - Web Developer
-              
-              </h1>
-            </Link>
-            
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <h2 className={utilStyles.headingLg}>
-              Steven Leabo - Web Developer
-              
-              </h2>
-            </Link>
-           
-          </>
-        )}
-      </header>
-      <main>
-      {children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back home</a>
-          </Link>
+    return (
+        <div>
+            <div className={styles.container} style={{ minHeight: "75vh" }}>
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com/"
+                        crossorigin
+                    />
+                    <link
+                        rel="dns-prefetch"
+                        href="https://fonts.gstatic.com/"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta
+                        name="description"
+                        content=" Landing Page for Steven Leabo - Web Developer"
+                    />
+                    <meta name="og:title" content={siteTitle} />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Head>
+                <header className={styles.header}>
+                    {home ? (
+                        <>
+                            <Link href="/">
+                                <h1 className={utilStyles.heading2Xl}>
+                                    Steven Leabo - Web Developer
+                                </h1>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/">
+                                <h2 className={utilStyles.heading2Xl}>
+                                    Steven Leabo - Web Developer
+                                </h2>
+                            </Link>
+                        </>
+                    )}
+                </header>
+                <main>{children}</main>
+                {!home && (
+                    <div className={styles.backToHome}>
+                        <Link href="/">
+                            <a>← Back home</a>
+                        </Link>
+                    </div>
+                )}
+            </div>
+            <div className={styles.container} style={{ marginBottom: "0" }}>
+                <footer className="footer">
+                    <div
+                        className={utilStyles.socialLinks}
+                        style={{ textAlign: "center" }}
+                    >
+                        <a
+                            rel="noopener"
+                            href="https://github.com/leabs"
+                            target="_blank"
+                            className="has-text-primary"
+                        >
+                            GitHub
+                        </a>
+                        <span> | </span>
+                        <a
+                            rel="noopener"
+                            href="https://codepen.io/leab"
+                            target="_blank"
+                            className="has-text-primary"
+                        >
+                            CodePen
+                        </a>
+                        <span> | </span>
+                        <a
+                            rel="noopener"
+                            href="https://www.linkedin.com/in/stevenleabo/"
+                            target="_blank"
+                            className="has-text-primary"
+                        >
+                            LinkedIn
+                        </a>
+                        <span> | </span>
+                        <a
+                            rel="noopener"
+                            href="https://www.upwork.com/o/profiles/users/~011442e16729fe0928/"
+                            target="_blank"
+                            className="has-text-primary"
+                        >
+                            Upwork
+                        </a>
+                    </div>
+                </footer>
+            </div>
         </div>
-      )}
-    </div>
-    <div className={styles.container} style={{ marginBottom: '0'}}>
-    <footer className="footer">
-    <div className={utilStyles.socialLinks} style={{ textAlign:"center"}}>
-    <a rel="noopener" href="https://github.com/leabs" target="_blank" className="has-text-primary">GitHub</a>
-      <span> | </span>
-      <a rel="noopener" href="https://codepen.io/leab" target="_blank" className="has-text-primary">CodePen</a>
-      <span> | </span>
-      <a rel="noopener" href="https://www.linkedin.com/in/stevenleabo/" target="_blank" className="has-text-primary">LinkedIn</a>
-      <span> | </span>
-      <a rel="noopener" href="https://www.upwork.com/o/profiles/users/~011442e16729fe0928/" target="_blank" className="has-text-primary">Upwork</a>
-      </div>
-    </footer>
-    </div>
-  </div>
-  )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,61 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import * as Sentry from "@sentry/react"
-import { Integrations } from "@sentry/tracing"
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: "https://6bf06797ba1142bd94951510f04b62da@o167874.ingest.sentry.io/1242251",
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
+    dsn:
+        "https://6bf06797ba1142bd94951510f04b62da@o167874.ingest.sentry.io/1242251",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
 });
 
-
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
 }
 
 export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-        Front-end web developer with over 4 years professional experience. I specialize in custom styling, fast secure static websites, and custom Wordpress sites. 
-        </p>
-      </section>
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>
+                    Front-end web developer with over 4 years professional
+                    experience. I specialize in custom styling, fast secure
+                    static websites, and custom Wordpress sites.
+                </p>
+            </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small> {' - '}
-            <Link href="/posts/[id]" as={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            
-          </li>
-          
-          ))}
-        </ul>
-      </section>
-
-     
-    </Layout>
-  )
+            <section
+                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+            >
+                <br />
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>{" "}
+                            {" - "}
+                            <Link href="/posts/[id]" as={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    );
 }
-
-
